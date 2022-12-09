cf61864cecb7648ed250e9f5ff4b5ad3304f160d
Otsile Molosiwa <otsilemolosiwa@gmail.com>
2022-12-09 13:02:40 +0200

    task 0 -3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#include "main.h"
-#include "unistd.h"
+#ifndef MAIN_H
+#define MAIN_H
 
 
 int _putchar(char c);
@@ -15,3 +15,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+
+#endif
